a8888c2e34e713253bf76a963a712bc2aa8646c4
shichishima <shichishima@users.noreply.github.com>
2026-04-25 23:44:11 +0900

    update settings text

--- main.ts
+++ main.ts
@@ -1168,7 +1168,7 @@ class UniversalCursorHotkeysSettingTab extends PluginSettingTab {
 			.setName('Smart HOME (advanced)')
 			.then(setting => this.setHtmlDesc(setting, '' +
 				'<b>ON:</b> Also skips past headings (<code>#</code>) and footnotes (<code>[^1]:</code>).<br>' +
-				'<i>Requires <b>Smart HOME (Standard)</b> to be enabled.</i>'))
+				'<i>Requires <b>Smart HOME (standard)</b> to be enabled.</i>'))
 			.addToggle(toggle => {
 				advancedToggle = toggle;
 				toggle.setValue(this.plugin.settings.smartHomeAdvanced)
